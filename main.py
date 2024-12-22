4d5ff70a31666606e4b541834c719b8b3c7fcb52
Ryan <r.benatallah01@gmail.com>
2024-12-22 12:08:48 +0100

    Class

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import asyncio
 from langchain_google_vertexai import ChatVertexAI
-from src.scout import generate_url, scrape_and_save
 from langchain.output_parsers.json import SimpleJsonOutputParser
-
+from src.scout import ResearchScraper
 
 if __name__ == "__main__":
     # Ensure your VertexAI credentials are configured
     model = ChatVertexAI(model="gemini-1.5-flash")
     # JSON Parser
     json_parser = SimpleJsonOutputParser()
+    # Instantiate the ResearchScraper
+    scraper = ResearchScraper(model=model, json_parser=json_parser)
     # Generate URLs
-    results = generate_url("Latest automotive technology innovations in 2024", json_parser=json_parser, model=model)
+    results = scraper.generate_url("Latest automotive technology innovations in 2024")
     urls = list(map(lambda x: x['url'], results['research_urls']))
     # Scrape URLs and save contents
-    asyncio.run(scrape_and_save(urls))
+    asyncio.run(scraper.scrape_and_save(urls))
